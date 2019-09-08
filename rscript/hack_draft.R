5c23b3f853b5ab6f36d5a999d2779ce8e285e36f
Klaus Schneider <klaus@cs.arizona.edu>
2019-09-08 10:47:33 -0400

    Update

--- rscript/hack_draft.R
+++ rscript/hack_draft.R
@@ -22,12 +22,7 @@ rates <- suppressMessages(read_tsv(str_c(folder,"rates.txt"), guess_max = 200000
 delay <- suppressMessages(read_tsv(str_c(folder,"delay.txt")))
 drop <- suppressMessages(read_tsv(str_c(folder,"drop.txt")))
 
-rates %<>%
-	filter(Node == N0)
-	# group_by(Time, Node) %>% 
-	# mutate(KbSum = sum(Kilobytes), CompSum = 10000.0 / KbSum, UtilSum= -1.0/(KbSum*8/1024))
-
-rates   %>% filter(Node == "N0", Type=="InData" )
+# rates   %>% filter(Node == "N0", Type=="InData" )
 
 (g.rates <- rates %>% filter(Node == "N0", Type=="OutData", FaceId==258 ) %>%
 		ggplot(aes (x=Time, y=(Kilobytes*8)/1024, color=FaceId)) +
@@ -39,36 +34,21 @@ rates   %>% filter(Node == "N0", Type=="InData" )
 		expand_limits(y=0) +
 		facet_wrap(~ Type))
 
-drop
-
-g.drop <- drop %>%
+(g.drop <- drop %>%
 	ggplot(aes (x=Time, y=Kilobytes)) +
-	geom_line(size=0.8)
+	geom_line(size=0.8))
 
-# rtt = as_tibble(read.csv(sep=c('\t', ' '),"rtt_smallq.txt"))
-# rtt2 <- gather(rtt, "type", "val", -segment)
 
 (g.delay <- delay %>% #filter(Type=="LastDelay") %>%
 	ggplot(aes(x=Time, y=DelayS*1000)) +
 	geom_line(size=0.8) +
-	ylab("RTT [s]") +
-	xlab("Time [ms]")) 
-	# geom_point(size=2) +
-
-# delay
-# g.cwnd <-  ggplot(data=cwnd, aes(x=time, y=cwndsize)) +
-# 	geom_line() +
-# 	geom_point(size=2) +
-# 	ylab("Cwnd") +
-# 	xlab("Time [s]") +
-# 	scale_shape_manual(values=seq(0,10))
-# # facet_grid(~ var)
-# g.cwnd
+	ylab("RTT [ms]") +
+	xlab("Time [m]")) 
 
 
 # height = 2.7, width=4.35
 ggsave("../plots/rates.pdf", g.rates, device=cairo_pdf)
+ggsave("../plots/delay.pdf", g.delay, device=cairo_pdf)
 ggsave("../plots/drop.pdf", g.drop, device=cairo_pdf)
-ggsave("../plots/cwnd.pdf", g.cwnd, device=cairo_pdf)
-ggsave("../plots/rtt.pdf", g.rtt, device=cairo_pdf)
+
 
