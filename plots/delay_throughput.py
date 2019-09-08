6b9a446e378a9beb0eb13aa6e11bd25f68dc2f92
Teng Liang <philoliang@email.arizona.edu>
2019-09-08 14:23:54 +0000

    delay and rate plots with tight layout

--- plots/delay_throughput.py
+++ plots/delay_throughput.py
@@ -1,15 +1,8 @@
 import matplotlib.pyplot as plt 
 import sys
 
-def main():
- 
-  if len(sys.argv) < 2:
-    print("one argument is required - [directory name]")
-    exit(1)
-  else:
-    dir_name = sys.argv[1]
-
-  delayFile = open(dir_name+"/delay.txt","r")
+def processDelay(fileName):
+  delayFile = open(fileName, "r")
   last_delays = []
   full_delays = []
   rtx_counts = []
@@ -19,7 +12,7 @@ def main():
     if c == 0:
       c = 1
       continue
-    elif c == 1000:
+    elif c == 10000:
       break
     else:
       c += 1
@@ -31,10 +24,55 @@ def main():
       full_delays.append(l[5])
       rtx_counts.append(l[7])
 
-  plt.plot([times[i] for i in range(len(times)) if i % 100 == 0], [full_delays[i] for i in range(len(full_delays)) if i % 100 == 0])
-  plt.xlabel('Time (s)')
-  plt.ylabel('Full Delays (s)')
+  flt_rate = 300
+  plt.figure(1)
+  plt.plot([times[i] for i in range(len(times)) if i % flt_rate == 0], [full_delays[i] for i in range(len(full_delays)) if i % flt_rate == 0])
+  plt.tight_layout()
+  plt.xlabel('time (s)')
+  plt.ylabel('full delays (s)')
   plt.savefig('delay.png')
+
+  plt.figure(2)
+  plt.plot([times[i] for i in range(len(times)) if i % flt_rate == 0], [full_delays[i] for i in range(len(full_delays)) if i % flt_rate == 0])
+  plt.tight_layout()
+  plt.xlabel('time (s)')
+  plt.ylabel('rtx count')
+  plt.savefig('rtx.png')
+
+def processDrop():
+  pass
+
+def processThroughput(fileName):
+  ratesFile = open(fileName, "r")
+  outDatas = []
+  times = []
+  c = 0
+  for each in ratesFile:
+    if "N0" in each and "appFace" in each and "OutData" in each:
+      c+=1
+      l = each.split()
+      times.append(l[0])
+      outDatas.append(l[6])      
+
+  max_rate = 190
+  plt.figure(3)
+  plt.plot([times[i] for i in range(len(times)) if i < max_rate], [outDatas[i] for i in range(len(outDatas)) if i < max_rate])
+  plt.tight_layout()
+  plt.xlabel('time (s)')
+  plt.ylabel('Data rates (s)')
+  plt.savefig('data_rates.png')
+
+def main():
+ 
+  if len(sys.argv) < 2:
+    print("one argument is required - [directory name]")
+    exit(1)
+  else:
+    dir_name = sys.argv[1]
+  
+  processDelay(dir_name+"delay.txt")
+  processThroughput(dir_name+"rates.txt")
+  processDrop()
 
 if __name__ == "__main__":
     main()
